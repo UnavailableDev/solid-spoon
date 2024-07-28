bf029e7a8f559d5caa128eb20cfd22734943c039
UnavailableDev <69792062+UnavailableDev@users.noreply.github.com>
2024-07-28 18:36:38 +0200

    gen_regex

--- lexer.c
+++ lexer.c
@@ -1,7 +1,20 @@
-#include <stdio.h>
-
 #include "lexer.h"
 
 void foo(){
-   printf("Hello lex!");
+   printf("Hello lex!\n");
+}
+
+
+regex_t* gen_regex(char* expr){
+   int ret_val;
+   regex_t* reg;
+
+   ret_val = regcomp(reg, expr, 0);
+
+   if (ret_val != 0){
+      printf("INTERN_ERR: during regex compile %x\n", ret_val);
+      return NULL;
+   }
+
+   return reg;
 }
--- lexer.h
+++ lexer.h
@@ -1,1 +1,8 @@
+#include <stdio.h>
+
+#include <regex.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
 void foo();
+regex_t* gen_regex(char* expr);
