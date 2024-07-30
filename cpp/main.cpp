e7cb430a331a04eee38b151c54113257db58e9d4
UnavailableDev <69792062+UnavailableDev@users.noreply.github.com>
2024-07-30 20:55:52 +0200

    fopen

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,14 +1,28 @@
 #include "lexer.h"
 
 #include <fstream>
+#include <iostream>
+
+#define READ_BUFFER 2048
 
 int main(int argc, char const *argv[]) {
-	foo();
+	if (argc < 2)
+		return 1;
+	// foo();
 
 
-	// fopen();
+	std::string filename{argv[1]};
+	std::fstream fp{filename};
 
+	if (!fp.is_open()) {
+		std::cout << "Failed to open: " << filename << std::endl;
+		return 2;
+	} 
 
+	char* file = (char*) malloc(sizeof(char)*READ_BUFFER);
+	fp.read(file, READ_BUFFER);
+	// std::cout << file;
+	
 
 	Lexer* l = new Lexer();
 	// l->init();
